c1b789723ceab8de10b7ebacf151b148f060d5c0
yousseftamer2003 <yousseftamer2003.yt@gmail.com>
2024-10-01 11:51:26 +0300

    pickup location

--- lib/views/admin/screens/pickup_location_screen.dart
+++ lib/views/admin/screens/pickup_location_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:transit_station/constants/colors.dart';
+import 'package:transit_station/views/admin/screens/add_pickup_location_screen.dart';
 import 'package:transit_station/views/admin/widgets/pickup_container.dart';
 import 'package:transit_station/views/auth_screens/widgets/build_appbar.dart';
 
@@ -11,19 +13,49 @@ class PickupLocationScreen extends StatelessWidget {
       appBar: buildAppBar(context, '#Pick-Up Location'),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: const [
-            PickupContainer(
-                image: 'assets/images/location 1.png',
-                title: 'location 1',
-                subtitle:'3rd floor, Apt. 5, AndalusiaBuilding, Al-Qasr Al-Aini Street',
-                ),
-                SizedBox(height: 20,),
-                PickupContainer(
-                image: 'assets/images/location 1.png',
-                title: 'location 2',
-                subtitle:'3rd floor, Apt. 5, AndalusiaBuilding, Al-Qasr Al-Aini Street',
-                ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: const [
+                  PickupContainer(
+                    image: 'assets/images/location 1.png',
+                    title: 'location 1',
+                    subtitle:
+                        '3rd floor, Apt. 5, AndalusiaBuilding, Al-Qasr Al-Aini Street',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PickupContainer(
+                    image: 'assets/images/location 1.png',
+                    title: 'location 2',
+                    subtitle:
+                        '3rd floor, Apt. 5, AndalusiaBuilding, Al-Qasr Al-Aini Street',
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx)=> const AddPickupLocationScreen())
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: defaultColor,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 18,
+                  )),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Icon(Icons.add), Text('Add')],
+              ),
+            ),
           ],
         ),
       ),
