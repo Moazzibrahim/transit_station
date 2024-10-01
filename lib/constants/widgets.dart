75e58883ed29d924b82e3a22c149b6b5a2f784f1
yousseftamer2003 <yousseftamer2003.yt@gmail.com>
2024-10-01 11:49:00 +0300

    widgets constants

--- lib/constants/widgets.dart
+++ lib/constants/widgets.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:transit_station/constants/colors.dart';
 
-Widget buildAppBar(BuildContext context,String title) {
-  return AppBar(
-    title: Text(title,style: const TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
-    leading: IconButton(onPressed: (){
-      Navigator.pop(context);
-    }, icon: const Icon(Icons.arrow_back_ios,color: defaultColor,)),
+
+InputDecoration inputDecoration(String hintText) {
+  return InputDecoration(
+    hintText: hintText,
+    hintStyle: const TextStyle(color: Colors.grey),
+    contentPadding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16.0),
+      borderSide: const BorderSide(
+        color: defaultColor, // Border color similar to the image
+        width: 1.0,
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16.0),
+      borderSide: const BorderSide(
+        color: defaultColor, // Same color as enabled but can be changed if needed
+        width: 1.5,
+      ),
+    ),
   );
 }
