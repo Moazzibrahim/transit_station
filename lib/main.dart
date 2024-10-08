ed22a25737e4530c8f32a391abe3c05ce353cd5a
yousseftamer2003 <yousseftamer2003.yt@gmail.com>
2024-10-08 16:19:18 +0300

    complete merge

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:transit_station/controllers/dashboard_controller.dart';
 import 'package:transit_station/controllers/expences_provider.dart';
+import 'package:transit_station/controllers/get_dropdown_subscriptions.dart';
 import 'package:transit_station/controllers/get_dropdowndata_provider.dart';
 import 'package:transit_station/controllers/get_profile_data.dart';
 import 'package:transit_station/controllers/image_services.dart';
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ParkingController()),
         ChangeNotifierProvider(create: (_) => RevenueProvider()),
         ChangeNotifierProvider(create: (_) => ExpencesProvider()),
+        ChangeNotifierProvider(create: (_) => GetDropdowndataSubscriptionProvider()),
       ],
       child: ScreenUtilInit(
         designSize: const Size(360, 690),
