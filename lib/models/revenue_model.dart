3ae907fb8b1209cf624dbcbb9a6cf626ffe97b1b
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-10-12 10:40:19 +0300

    update  model

--- lib/models/revenue_model.dart
+++ lib/models/revenue_model.dart
@@ -4,18 +4,23 @@ class Revenue {
   final double amount;
   final String date;
 
-  Revenue(
-      {required this.type,
-      required this.id,
-      required this.amount,
-      required this.date});
+  Revenue({
+    required this.type,
+    required this.id,
+    required this.amount,
+    required this.date,
+  });
 
-  factory Revenue.fromJson(Map<String, dynamic> json) => Revenue(
-        type: json['type'],
-        id: json['id'],
-        amount: json['revenue_amount'].toDouble(),
-        date: json['date'],
-      );
+  factory Revenue.fromJson(Map<String, dynamic> json) {
+    return Revenue(
+      type: json['type'] ?? '',
+      id: json['id'] ?? 0, // Providing default value of 0 if id is null
+      amount: (json['revenue_amount'] as num?)?.toDouble() ??
+          0.0, // Safely handling potential null and converting to double
+      date: json['date'] ??
+          '', // Providing default value of empty string if date is null
+    );
+  }
 }
 
 class Revenues {
@@ -23,16 +28,26 @@ class Revenues {
 
   Revenues({required this.revenues});
 
-  factory Revenues.fromJson(Map<String,dynamic> json)=> Revenues(revenues: json['revenues']);
+  factory Revenues.fromJson(Map<String, dynamic> json) =>
+      Revenues(revenues: json['revenues']);
 }
 
 class RevenueType {
   final String name;
   final int id;
 
-  RevenueType({required this.name, required this.id});
+  RevenueType({
+    required this.name,
+    required this.id,
+  });
 
-  factory RevenueType.fromJson(Map<String,dynamic> json)=> RevenueType(name: json['type_name'], id: json['id']);
+  factory RevenueType.fromJson(Map<String, dynamic> json) {
+    return RevenueType(
+      name: json['type_name'] ??
+          '', // Default to an empty string if 'type_name' is null
+      id: json['id'] ?? 0, // Default to 0 if 'id' is null
+    );
+  }
 }
 
 class RevenueTypes {
@@ -40,5 +55,6 @@ class RevenueTypes {
 
   RevenueTypes({required this.revenueTypes});
 
-  factory RevenueTypes.fromJson(Map<String,dynamic> json)=> RevenueTypes(revenueTypes: json['key']);
+  factory RevenueTypes.fromJson(Map<String, dynamic> json) =>
+      RevenueTypes(revenueTypes: json['key']);
 }
