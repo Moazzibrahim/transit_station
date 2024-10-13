002403d5e5939f3af321db335eba8a19d66d7edf
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-10-13 10:19:33 +0300

    expenses done

--- lib/controllers/expences_provider.dart
+++ lib/controllers/expences_provider.dart
@@ -8,39 +8,71 @@ import 'package:transit_station/controllers/login_provider.dart';
 import 'package:transit_station/models/expences_model.dart';
 import 'package:http/http.dart' as http;
 
-
-class ExpencesProvider with ChangeNotifier{
+class ExpencesProvider with ChangeNotifier {
   List<Expence> _expenceData = [];
-  List<Expence> get expenceData=> _expenceData;
+  List<Expence> _filteredexpenses = [];
+
+  List<Expence> get expenceData =>
+      _filteredexpenses.isEmpty ? _expenceData : _filteredexpenses;
 
   bool isTypeAdded = false;
   bool isExpenceAdded = false;
 
   List<ExpenceType> _expenceTypesData = [];
-  List<ExpenceType> get expenceTypesData=> _expenceTypesData;
+  List<ExpenceType> get expenceTypesData => _expenceTypesData;
+  void filterExpencesByDate(String filter) {
+    DateTime now = DateTime.now();
+    _filteredexpenses =
+        List.from(_expenceData); // Initialize from the original data
 
-  Future<void> addTypeExpence(BuildContext context,String name) async{
-    try {
-    final tokenProvider = Provider.of<TokenModel>(context, listen: false);
-    final token = tokenProvider.token;
-    final response = await http.post(Uri.parse('https://transitstation.online/api/admin/expence/addtype'),
-    headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Bearer $token',
-    },
-    body: jsonEncode({
-      'type_name' : name,
-    })
-    );
-    if(response.statusCode == 200){
-      isTypeAdded = true;
-      notifyListeners();
-    }else {
-      log('status code: ${response.statusCode}');
-      isTypeAdded = false;
-      notifyListeners();
+    if (filter == 'Yearly') {
+      _filteredexpenses = _filteredexpenses.where((expence) {
+        DateTime expenceDate = DateTime.parse(expence.date);
+        return expenceDate.year == now.year; // Match current year (e.g., 2024)
+      }).toList();
+    } else if (filter == 'Monthly') {
+      _filteredexpenses = _filteredexpenses.where((expence) {
+        DateTime expenceDate = DateTime.parse(expence.date);
+        return expenceDate.year == now.year &&
+            expenceDate.month == now.month; // Current month
+      }).toList();
+    } else if (filter == 'Weekly') {
+      _filteredexpenses = _filteredexpenses.where((expence) {
+        DateTime expenceDate = DateTime.parse(expence.date);
+        final currentWeekStart = now.subtract(Duration(days: now.weekday - 1));
+        final currentWeekEnd = currentWeekStart.add(const Duration(days: 7));
+        return expenceDate.isAfter(currentWeekStart) &&
+            expenceDate.isBefore(currentWeekEnd);
+      }).toList();
+    } else {
+      _filteredexpenses = List.from(_expenceData); // Show all data for 'All'
     }
+
+    notifyListeners(); // Notify after filtering is complete
+  }
+
+  Future<void> addTypeExpence(BuildContext context, String name) async {
+    try {
+      final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+      final token = tokenProvider.token;
+      final response = await http.post(
+          Uri.parse('https://transitstation.online/api/admin/expence/addtype'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: jsonEncode({
+            'type_name': name,
+          }));
+      if (response.statusCode == 200) {
+        isTypeAdded = true;
+        notifyListeners();
+      } else {
+        log('status code: ${response.statusCode}');
+        isTypeAdded = false;
+        notifyListeners();
+      }
     } catch (e) {
       log('add type revenue: $e');
       isTypeAdded = false;
@@ -48,85 +80,96 @@ class ExpencesProvider with ChangeNotifier{
     }
   }
 
-  Future<void> fetchExpences(BuildContext context) async{
+  Future<void> fetchExpences(BuildContext context) async {
     try {
-    final tokenProvider = Provider.of<TokenModel>(context, listen: false);
-    final token = tokenProvider.token;
-    final response =await http.get(Uri.parse('https://transitstation.online/api/admin/expence'),
-    headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Bearer $token',
-    },
-    );
-    if(response.statusCode ==200){
-      Map<String,dynamic> responseData = jsonDecode(response.body);
-      final Expences expences = Expences.fromJson(responseData);
-      _expenceData = expences.expences.map((e) => Expence.fromJson(e),).toList();
-      notifyListeners();
-    }else{
-      log('error with status code: ${response.statusCode}');
-    }
+      final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+      final token = tokenProvider.token;
+      final response = await http.get(
+        Uri.parse('https://transitstation.online/api/admin/expence'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseData = jsonDecode(response.body);
+        final Expences expences = Expences.fromJson(responseData);
+        _expenceData = expences.expences
+            .map(
+              (e) => Expence.fromJson(e),
+            )
+            .toList();
+        notifyListeners();
+      } else {
+        log('error with status code: ${response.statusCode}');
+      }
     } catch (e) {
       log('error in fetch dashboard data: $e');
     }
   }
 
-  Future<void> addExpence(BuildContext context, DateTime date, int typeId, double amount) async {
-  try {
-    String formattedDate = DateFormat('yyyy-MM-dd').format(date);
+  Future<void> addExpence(
+      BuildContext context, DateTime date, int typeId, double amount) async {
+    try {
+      String formattedDate = DateFormat('yyyy-MM-dd').format(date);
 
-    final tokenProvider = Provider.of<TokenModel>(context, listen: false);
-    final token = tokenProvider.token;
+      final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+      final token = tokenProvider.token;
 
-    final response = await http.post(
-      Uri.parse('https://transitstation.online/api/admin/expence/add'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode({
-        'date': formattedDate,
-        'type_expence_id': typeId,
-        'revenue_amount': amount,
-      }),
-    );
+      final response = await http.post(
+        Uri.parse('https://transitstation.online/api/admin/expence/add'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({
+          'date': formattedDate,
+          'type_expence_id': typeId,
+          'revenue_amount': amount,
+        }),
+      );
 
-    if (response.statusCode == 200) {
-      isExpenceAdded = true;
-      notifyListeners();
-    } else {
-      log('status code: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        isExpenceAdded = true;
+        notifyListeners();
+      } else {
+        log('status code: ${response.statusCode}');
+        isExpenceAdded = false;
+        notifyListeners();
+      }
+    } catch (e) {
+      log('add type expence: $e');
       isExpenceAdded = false;
       notifyListeners();
     }
-  } catch (e) {
-    log('add type expence: $e');
-    isExpenceAdded = false;
-    notifyListeners();
   }
-}
 
-Future<void> fetchExpenceTypes(BuildContext context) async{
+  Future<void> fetchExpenceTypes(BuildContext context) async {
     try {
-    final tokenProvider = Provider.of<TokenModel>(context, listen: false);
-    final token = tokenProvider.token;
-    final response =await http.get(Uri.parse('https://transitstation.online/api/admin/revenue/types'),
-    headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Bearer $token',
-    },
-    );
-    if(response.statusCode ==200){
-      Map<String,dynamic> responseData = jsonDecode(response.body);
-      final ExpenceTypes expenceTypes = ExpenceTypes.fromJson(responseData);
-      _expenceTypesData = expenceTypes.expenceTypes.map((e) => ExpenceType.fromJson(e),).toList();
-      notifyListeners();
-    }else{
-      log('error with status code: ${response.statusCode}');
-    }
+      final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+      final token = tokenProvider.token;
+      final response = await http.get(
+        Uri.parse('https://transitstation.online/api/admin/revenue/types'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseData = jsonDecode(response.body);
+        final ExpenceTypes expenceTypes = ExpenceTypes.fromJson(responseData);
+        _expenceTypesData = expenceTypes.expenceTypes
+            .map(
+              (e) => ExpenceType.fromJson(e),
+            )
+            .toList();
+        notifyListeners();
+      } else {
+        log('error with status code: ${response.statusCode}');
+      }
     } catch (e) {
       log('error in fetch types data: $e');
     }
--- lib/views/admin/screens/expences_screen.dart
+++ lib/views/admin/screens/expences_screen.dart
@@ -13,129 +13,166 @@ class ExpencesScreen extends StatefulWidget {
 }
 
 class _ExpencesScreenState extends State<ExpencesScreen> {
+  String _selectedFilter =
+      'All'; // Filter options: All, Weekly, Monthly, Yearly
+
   @override
   void initState() {
-    Provider.of<ExpencesProvider>(context,listen: false).fetchExpences(context);
+    Provider.of<ExpencesProvider>(context, listen: false)
+        .fetchExpences(context);
     super.initState();
   }
+
+  void _applyFilter() {
+    // Apply the filter and notify the listeners
+    Provider.of<ExpencesProvider>(context, listen: false)
+        .filterExpencesByDate(_selectedFilter);
+
+    // Refresh the state to update the UI
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-    centerTitle: true,
-    title: const Text(
-      'Excpences',
-      style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-    ),
-    leading: Container(
-      margin: const EdgeInsets.all(6),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-      child: IconButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        icon: const Icon(
-          Icons.arrow_back_ios,
-          color: defaultColor,
+        centerTitle: true,
+        title: const Text(
+          'Expenses',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
         ),
-      ),
-    ),
-    actions: [
-      ElevatedButton(
-      onPressed: (){
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (ctx)=> const AddTypeExpenceScreen())
-        );
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: defaultColor,
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16)
-        )
-      ),
-      child: const Row(
-        children: [
-          Icon(Icons.add),
-          Text('Type'),
+        leading: Container(
+          margin: const EdgeInsets.all(6),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+          child: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: defaultColor,
+            ),
+          ),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => const AddTypeExpenceScreen()));
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: defaultColor,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16))),
+            child: const Row(
+              children: [
+                Icon(Icons.add),
+                Text('Type'),
+              ],
+            ),
+          ),
+          const SizedBox(width: 3),
         ],
       ),
-      ),
-      const SizedBox(width: 3,)
-    ],
-  ),
-        body: Consumer<ExpencesProvider>(
+      body: Consumer<ExpencesProvider>(
         builder: (context, expenceProvider, _) {
-          if(expenceProvider.expenceData.isEmpty){
+          if (expenceProvider.expenceData.isEmpty) {
             return const Center(
               child: CircularProgressIndicator(),
             );
-          }else{
+          } else {
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton<String>(
+                      value: _selectedFilter,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedFilter = newValue!;
+                        });
+                        _applyFilter();
+                      },
+                      items: <String>['All', 'Weekly', 'Monthly', 'Yearly']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      icon: const Icon(Icons.filter_list),
+                    ),
+                  ),
                   Expanded(
                     child: ListView.builder(
-                      itemCount:expenceProvider.expenceData.length ,
-                              itemBuilder: (context, index) {
-                    final expence = expenceProvider.expenceData[index];
-                    return Card(
-                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
+                      itemCount: expenceProvider.expenceData.length,
+                      itemBuilder: (context, index) {
+                        final expence = expenceProvider.expenceData[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 8),
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          color: Colors.grey[100],
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _buildInfoRow(
+                                  icon: Icons.date_range,
+                                  label: 'Date',
+                                  value: expence.date,
+                                ),
+                                const SizedBox(height: 8),
+                                _buildInfoRow(
+                                  icon: Icons.money,
+                                  label: 'Amount',
+                                  value: expence.amount.toString(),
+                                ),
+                                const SizedBox(height: 8),
+                                _buildInfoRow(
+                                  icon: Icons.category,
+                                  label: 'Type',
+                                  value: expence.type,
+                                ),
+                              ],
                             ),
-                            color: Colors.grey[100],
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            _buildInfoRow(
-                                    icon: Icons.date_range,
-                                    label: 'Date',
-                                    value: expence.date,
-                                  ),
-                                  const SizedBox(height: 8),
-                                  _buildInfoRow(
-                                    icon: Icons.money,
-                                    label: 'Amount',
-                                    value: expence.amount.toString(),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  _buildInfoRow(
-                                    icon: Icons.category,
-                                    label: 'Type',
-                                    value: expence.type,
-                                  ),
-                          ],
-                        ),
-                      ),
-                    );
-                              },
-                              ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                   ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-          MaterialPageRoute(builder: (ctx)=> const AddExpenceScreen())
-        );
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: defaultColor,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 18,
-                      )),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Icon(Icons.add), Text('Add',style: TextStyle(fontSize: 20),)],
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (ctx) => const AddExpenceScreen()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: defaultColor,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                        )),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.add),
+                        Text(
+                          'Add',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
                 ],
               ),
             );
@@ -144,6 +181,7 @@ class _ExpencesScreenState extends State<ExpencesScreen> {
       ),
     );
   }
+
   Widget _buildInfoRow(
       {required IconData icon, required String label, required String value}) {
     return Row(
